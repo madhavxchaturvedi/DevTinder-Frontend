b0deee802f6e7812248328f6cc43de7fa4eadf21
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-07-24 00:33:32 +0530

    fix: Feed Slice

--- src/redux/feedSlice.js
+++ src/redux/feedSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const feedSlice = createSlice({
-  name: feed,
+  name: "feed",
   initialState: null,
   reducers: {
     addFeed: (state, action) => {
